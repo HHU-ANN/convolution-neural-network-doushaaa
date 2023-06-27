b449e6e94b6264c9a54e61a5309d3cb414b8998d
doushaaa <122351819+doushaaa@users.noreply.github.com>
2023-06-27 10:19:46 +0800

    Update Convolution_Neural_Network.py

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -1,128 +1,199 @@
-# 在该文件NeuralNetwork类中定义你的模型 
-# 在自己电脑上训练好模型，保存参数，在这里读取模型参数（不要使用JIT读取），在main中返回读取了模型参数的模型
-
 import os
-
 os.system("sudo pip3 install torch")
 os.system("sudo pip3 install torchvision")
-
-# !/usr/bin/python3
-# -*- coding:utf-8 -*-
-# Author:WeiFeng Liu
-# @Time: 2021/11/2 下午3:25
-
+import torch
 import torch.nn as nn
+from torch import optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision import datasets
+from tqdm import tqdm
+import torchvision
+
+'''定义超参数'''
+batch_size = 256  # 批的大小
+learning_rate = 1e-2  # 学习率
+num_epoches = 5  # 遍历训练集的次数
+
+'''
+transform = transforms.Compose([
+    transforms.RandomSizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize(mean = [ 0.485, 0.456, 0.406 ],
+                         std  = [ 0.229, 0.224, 0.225 ]),
+    ])
+'''
+
+'''下载训练集 CIFAR-10 10分类训练集'''
+train_dataset = datasets.CIFAR10('./data/exp03', train=True, transform=transforms.ToTensor(), download=True)
+train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+test_dataset = datasets.CIFAR10('./data/exp03', train=False, transform=transforms.ToTensor(), download=True)
+test_loader = DataLoader(test_dataset, batch_size=256, shuffle=False)
+
+'''定义网络模型'''
 
 
 class NeuralNetwork(nn.Module):
-    def __init__(self, width_mult=1):
+    def __init__(self, num_classes=10):
         super(NeuralNetwork, self).__init__()
-        # 定义每一个就卷积层
-        self.layer1 = nn.Sequential(
-            # 卷积层  #输入图像为1*28*28
-            nn.Conv2d(3, 32, kernel_size=3, padding=1),
-            # 池化层
-            nn.MaxPool2d(kernel_size=2, stride=2),  # 池化层特征图通道数不改变，每个特征图的分辨率变小
-            # 激活函数Relu
-            nn.ReLU(inplace=True),
-        )
-
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+        self.features = nn.Sequential(
+            # 1
+            nn.Conv2d(3, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
+            # 2
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
-        )
-
-        self.layer3 = nn.Sequential(
+            # 3
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
-        )
-
-        self.layer4 = nn.Sequential(
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            # 4
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 5
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
-        )
-        self.layer5 = nn.Sequential(
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # 6
             nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            # 7
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 8
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # 9
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # 10
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            # 11
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # 12
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            # 13
+            nn.Conv2d(512, 512, kernel_size=3, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.AvgPool2d(kernel_size=1, stride=1),
         )
-
-        # 定义全连接层
-        self.fc1 = nn.Linear(256 * 3 * 3, 1024)
-        self.fc2 = nn.Linear(1024, 512)
-        self.fc3 = nn.Linear(512, 10)
-        # 对应十个类别的输出
+        self.classifier = nn.Sequential(
+            # 14
+            nn.Linear(512, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            # 15
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            # 16
+            nn.Linear(4096, num_classes),
+        )
+        # self.classifier = nn.Linear(512, 10)
 
     def forward(self, x):
-        x = self.layer1(x)
-        x = self.layer2(x)
-        x = self.layer3(x)
-        x = self.layer4(x)
-        x = self.layer5(x)
-        # print(x.shape)
-        x = x.view(-1, 256 * 3 * 3)
-        x = self.fc1(x)
-        x = self.fc2(x)
-        x = self.fc3(x)
-
-        return x
+        out = self.features(x)
+        #        print(out.shape)
+        out = out.view(out.size(0), -1)
+        #        print(out.shape)
+        out = self.classifier(out)
+        #        print(out.shape)
+        return out
 
 
-# !/usr/bin/python3
-# -*- coding:utf-8 -*-
-# Author:WeiFeng Liu
-# @Time: 2021/11/4 下午12:59
+'''创建model实例对象，并检测是否支持使用GPU'''
+model = NeuralNetwork()
+use_gpu = torch.cuda.is_available()  # 判断是否有GPU加速
+if use_gpu:
+    model = model.cuda()
 
+'''定义loss和optimizer'''
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=learning_rate)
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torchvision
-from torchvision import transforms
-# 定义使用GPU
-from torch.utils.data import DataLoader
+'''训练模型'''
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+for epoch in range(num_epoches):
+    print('*' * 25, 'epoch {}'.format(epoch + 1), '*' * 25)  # .format为输出格式，formet括号里的即为左边花括号的输出
+    running_loss = 0.0
+    running_acc = 0.0
+    for i, data in tqdm(enumerate(train_loader, 1)):
 
-# 设置超参数
-epochs = 50
-batch_size = 256
-lr = 0.01
-
-transform = transforms.Compose([
-    transforms.Resize([32, 32]),
-    transforms.RandomHorizontalFlip(),
-    transforms.ToTensor(),
-    # transforms.Normalize(mean=[.5,.5,.5],std=[.5,.5,.5]),
-])
-
-train_dataset = torchvision.datasets.CIFAR10(root='./data', transform=transform, download=True)
-val_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform, download=True)
-# print(train_dataset[0])
-train_loader = DataLoader(train_dataset,
-                          batch_size=batch_size,
-                          shuffle=True, )
-val_loader = DataLoader(val_dataset, batch_size=512, shuffle=False)
-net = NeuralNetwork().cuda(device)
-loss_func = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.9)
-
-train_loss = []
-for epoch in range(epochs):
-    sum_loss = 0
-    for batch_idx, (x, y) in enumerate(train_loader):
-        x = x.to(device)
-        y = y.to(device)
-        pred = net(x)
-
+        img, label = data
+        # cuda
+        if use_gpu:
+            img = img.cuda()
+            label = label.cuda()
+        img = Variable(img)
+        label = Variable(label)
+        # 向前传播
+        out = model(img)
+        loss = criterion(out, label)
+        running_loss += loss.item() * label.size(0)
+        _, pred = torch.max(out, 1)  # 预测最大值所在的位置标签
+        num_correct = (pred == label).sum()
+        accuracy = (pred == label).float().mean()
+        running_acc += num_correct.item()
+        # 向后传播
         optimizer.zero_grad()
-        loss = loss_func(pred, y)
         loss.backward()
         optimizer.step()
-        sum_loss += loss.item()
-        train_loss.append(loss.item())
-        print(["epoch:%d , batch:%d , loss:%.3f" % (epoch, batch_idx, loss.item())])
-    torch.save(net.state_dict(), r'C:\Users\11620\convolution-neural-network-doushaaa\pth\model.pth')
+    print('Finish {} epoch, Loss: {:.6f}, Acc: {:.6f}'.format(
+        epoch + 1, running_loss / (len(train_dataset)), running_acc / (len(train_dataset))))
 
+    model.eval()  # 模型评估
+    eval_loss = 0
+    eval_acc = 0
+    for data in test_loader:  # 测试模型
+        img, label = data
+        if use_gpu:
+            img = Variable(img, volatile=True).cuda()
+            label = Variable(label, volatile=True).cuda()
+        else:
+            img = Variable(img, volatile=True)
+            label = Variable(label, volatile=True)
+        out = model(img)
+        loss = criterion(out, label)
+        eval_loss += loss.item() * label.size(0)
+        _, pred = torch.max(out, 1)
+        num_correct = (pred == label).sum()
+        eval_acc += num_correct.item()
+    print('Test Loss: {:.6f}, Acc: {:.6f}'.format(eval_loss / (len(
+        test_dataset)), eval_acc / (len(test_dataset))))
+    print()
+
+# 保存模型
+torch.save(model .state_dict(), r'C:\Users\11620\convolution-neural-network-doushaaa\pth\model.pth')
+
+def read_data():
+    # 这里可自行修改数据预处理，batch大小也可自行调整
+    # 保持本地训练的数据读取和这里一致
+    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
+    data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
+    return dataset_train, dataset_val, data_loader_train, data_loader_val
 
 def main():
     model = NeuralNetwork()  # 若有参数则传入参数
